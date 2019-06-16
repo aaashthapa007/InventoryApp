ed9d1e9630c21ced2104424afcd06fd490ab1b7e
aaashthapa007 <aaashthapa007@gmail.com>
2019-06-16 12:29:30 +0545

    dfgtyudfghj

--- app/src/main/java/com/example/inventoryapp/Fragment/Register.java
+++ app/src/main/java/com/example/inventoryapp/Fragment/Register.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.inventoryapp.R;
 
@@ -14,17 +16,38 @@ import com.example.inventoryapp.R;
  */
 public class Register extends Fragment {
 
-
-    public Register() {
         // Required empty public constructor
-    }
+        EditText EditTextFirstname, EditTextLastname, EditTextCompany, EditTextEmail, EditTextPassword;
+        Button buttonRegister;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        View v = inflater.inflate(R.layout.fragment_register, container, false);
+        EditTextFirstname = v.findViewById(R.id.registerEtFname);
+        EditTextLastname = v.findViewById(R.id.registerEtLname);
+        EditTextCompany = v.findViewById(R.id.registerEtComaanyName);
+        EditTextEmail = v.findViewById(R.id.registerEtEmail);
+        EditTextPassword = v.findViewById(R.id.registerEtPassword);
+        buttonRegister = v.findViewById(R.id.registerBtnRegister);
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Firstname = EditTextFirstname.getText().toString();
+                String Lastname = EditTextLastname.getText().toString();
+                String CompanyName = EditTextCompany.getText().toString();
+                String Email = EditTextEmail.getText().toString();
+                String Password = EditTextPassword.getText().toString();
+
+            }
+        });
+        
+        
+        return v;
+        
     }
 
 }
